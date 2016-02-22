ea460d8836bc9b6f65b18d2d878301c0ed6022f7
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-02-22 08:47:48 +0200

    upgrade to Hibernate 5 and JPA 2.1

--- framework/src/play/db/jpa/HibernateInterceptor.java
+++ framework/src/play/db/jpa/HibernateInterceptor.java
@@ -5,76 +5,25 @@ import org.hibernate.CallbackException;
 import org.hibernate.EmptyInterceptor;
 import org.hibernate.collection.spi.PersistentCollection;
 import org.hibernate.type.Type;
+
 import java.io.Serializable;
 
 
 public class HibernateInterceptor extends EmptyInterceptor {
 
-  public HibernateInterceptor() {
+    public HibernateInterceptor() {
 
-  }
-  
-  @Override
-  public int[] findDirty(Object o, Serializable id, Object[] arg2, Object[] arg3, String[] arg4, Type[] arg5) {
-    if (o instanceof JPABase && !((JPABase) o).willBeSaved) {
-      return new int[0];
-    }
-    return null;
-  }
-
-    @Override
-    public boolean onCollectionUpdate(Object collection, Serializable key) throws CallbackException {
-        if (collection instanceof PersistentCollection) {
-            Object o = ((PersistentCollection) collection).getOwner();
-            if (o instanceof JPABase) {
-                if (entities.get() != null) {
-                    return ((JPABase) o).willBeSaved || ((JPABase) entities.get()).willBeSaved;
-                } else {
-                    return ((JPABase) o).willBeSaved;
-                }
-            }
-        } else {
-            System.out.println("HOO: Case not handled !!!");
-        }
-        return super.onCollectionUpdate(collection, key);
     }
 
     @Override
-    public boolean onCollectionRecreate(Object collection, Serializable key) throws CallbackException {
-        if (collection instanceof PersistentCollection) {
-            Object o = ((PersistentCollection) collection).getOwner();
-            if (o instanceof JPABase) {
-                if (entities.get() != null) {
-                    return ((JPABase) o).willBeSaved || ((JPABase) entities.get()).willBeSaved;
-                } else {
-                    return ((JPABase) o).willBeSaved;
-                }
-            }
-        } else {
-            System.out.println("HOO: Case not handled !!!");
+    public int[] findDirty(Object o, Serializable id, Object[] arg2, Object[] arg3, String[] arg4, Type[] arg5) {
+        if (o instanceof JPABase && !((JPABase) o).willBeSaved) {
+            return new int[0];
         }
-
-        return super.onCollectionRecreate(collection, key);
+        return null;
     }
-
-    @Override
-    public boolean onCollectionRemove(Object collection, Serializable key) throws CallbackException {
-        if (collection instanceof PersistentCollection) {
-            Object o = ((PersistentCollection) collection).getOwner();
-            if (o instanceof JPABase) {
-                if (entities.get() != null) {
-                    return ((JPABase) o).willBeSaved || ((JPABase) entities.get()).willBeSaved;
-                } else {
-                    return ((JPABase) o).willBeSaved;
-                }
-            }
-        } else {
-            System.out.println("HOO: Case not handled !!!");
-        }
-        return super.onCollectionRemove(collection, key);
-    }
-
-    protected ThreadLocal<Object> entities = new ThreadLocal<Object>();
+    
+    private static final ThreadLocal<Object> entities = new ThreadLocal<Object>();
 
     @Override
     public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
